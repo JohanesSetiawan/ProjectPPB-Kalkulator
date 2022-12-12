2e7a7e5b0408759ef913477d4c028205b74b7eca
Johanes14 <setiawanjohanes41@gmail.com>
2022-12-12 10:58:24 +0700

    UAS-Kalkulator

--- app/src/main/java/com/example/kalkualtoruas/MainActivity.java
+++ app/src/main/java/com/example/kalkualtoruas/MainActivity.java
@@ -1,14 +1,151 @@
 package com.example.kalkualtoruas;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<ItemList> mExampleList;
+    private RecyclerView mRecyclerView;
+    private SharedPreferenceAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    RadioGroup operasiGroup;
+    RadioButton tambahRadio, kurangRadio, kaliRadio, bagiRadio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadData();
+        buildRecyclerView();
+        setInsertButton();
+
+        Button buttonDelete = findViewById(R.id.button_delete);
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteData();
+            }
+        });
+
+        operasiGroup = findViewById(R.id.operasiGroup);
+        tambahRadio = findViewById(R.id.tambahRadio);
+        kurangRadio = findViewById(R.id.kurangRadio);
+        kaliRadio = findViewById(R.id.kaliRadio);
+        bagiRadio = findViewById(R.id.bagiRadio);
+
+        operasiGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (tambahRadio.isChecked()) {
+                    Toast.makeText(MainActivity.this, "Tambah", Toast.LENGTH_SHORT).show();
+                } else if (kurangRadio.isChecked()) {
+                    Toast.makeText(MainActivity.this, "Kurang", Toast.LENGTH_SHORT).show();
+                } else if (kaliRadio.isChecked()) {
+                    Toast.makeText(MainActivity.this, "Kali", Toast.LENGTH_SHORT).show();
+                } else if (bagiRadio.isChecked()) {
+                    Toast.makeText(MainActivity.this, "Bagi", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
+    private void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(mExampleList);
+        editor.putString("task list", json);
+        editor.apply();
+    }
+
+    private void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("task list", null);
+        Type type = new TypeToken<ArrayList<ItemList>>() {}.getType();
+        mExampleList = gson.fromJson(json, type);
+
+        if (mExampleList == null) {
+            mExampleList = new ArrayList<>();
+        }
+    }
+
+    private void buildRecyclerView() {
+        mRecyclerView = findViewById(R.id.recyclerview);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new SharedPreferenceAdapter(mExampleList);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    private void setInsertButton() {
+        Button buttonInsert = findViewById(R.id.button_insert);
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText line1 = findViewById(R.id.edittext_line_1);
+                EditText line2 = findViewById(R.id.edittext_line_2);
+                insertItem(line1.getText().toString(), line2.getText().toString());
+                saveData();
+            }
+        });
+    }
+
+    private void insertItem(String line1, String line2) {
+        int hasil = 0;
+        if (tambahRadio.isChecked()) {
+            hasil = Integer.parseInt(line1) + Integer.parseInt(line2);
+        } else if (kurangRadio.isChecked()) {
+            hasil = Integer.parseInt(line1) - Integer.parseInt(line2);
+        } else if (kaliRadio.isChecked()) {
+            hasil = Integer.parseInt(line1) * Integer.parseInt(line2);
+        } else if (bagiRadio.isChecked()) {
+            hasil = Integer.parseInt(line1) / Integer.parseInt(line2);
+        }
+
+        // memunculkan operasi yang dipilih dari operasiGroup ke dalam hasil
+        String operasi = "";
+        if (tambahRadio.isChecked()) {
+            operasi = "+";
+        } else if (kurangRadio.isChecked()) {
+            operasi = "-";
+        } else if (kaliRadio.isChecked()) {
+            operasi = "x";
+        } else if (bagiRadio.isChecked()) {
+            operasi = ":";
+        }
+
+        mExampleList.add(new ItemList(line1, operasi, line2, String.valueOf(hasil)));
+        mAdapter.notifyItemInserted(mExampleList.size());
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private void deleteData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.apply();
+        mExampleList.clear();
+        mAdapter.notifyDataSetChanged();
+    }
+
 }
---
+++ app/src/main/java/com/example/kalkualtoruas/SharedPreferenceAdapter.java
@@ -0,0 +1,60 @@
+package com.example.kalkualtoruas;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class SharedPreferenceAdapter extends RecyclerView.Adapter<SharedPreferenceAdapter.ExampleViewHolder> {
+    private final ArrayList<ItemList> mExampleList;
+
+    public static class ExampleViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTextViewLine1;
+        public TextView mTextViewLineOperasi;
+        public TextView mTextViewLine2;
+        public TextView mTextViewLineHasil;
+
+        @SuppressLint("CutPasteId")
+        public ExampleViewHolder(View itemView) {
+            super(itemView);
+            mTextViewLine1 = itemView.findViewById(R.id.textview_line1);
+            mTextViewLineOperasi = itemView.findViewById(R.id.textview_operasi);
+            mTextViewLine2 = itemView.findViewById(R.id.textview_line2);
+            mTextViewLineHasil = itemView.findViewById(R.id.textview_hasil);
+        }
+    }
+
+    public SharedPreferenceAdapter(ArrayList<ItemList> exampleList) {
+        mExampleList = exampleList;
+    }
+
+    @NonNull
+    @Override
+    public ExampleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list, parent, false);
+        ExampleViewHolder evh = new ExampleViewHolder(v);
+        return evh;
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onBindViewHolder(ExampleViewHolder holder, int position) {
+        ItemList currentItem = mExampleList.get(position);
+
+        holder.mTextViewLine1.setText(currentItem.getLine1());
+        holder.mTextViewLine2.setText(currentItem.getLine2());
+        holder.mTextViewLineOperasi.setText(currentItem.getLineOperasi());
+        holder.mTextViewLineHasil.setText(currentItem.getLineHasil());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mExampleList.size();
+    }
+}
